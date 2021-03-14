6e9abe0d24d70eb606e5666c244b544ac8474b7d
Алексей Оникиенко <69063312+nedevan@users.noreply.github.com>
2021-03-14 21:43:53 +0300

    Update SpaceEnginerAirLock.cs
    Подвёл в удобочитаемы стиль

--- SpaceEnginer Scripts/SpaceEnginerAirLock.cs
+++ SpaceEnginer Scripts/SpaceEnginerAirLock.cs
@@ -19,11 +19,17 @@ void Main()
 
     // Двери
     text += "Двери:\n";
-    if(MainDoor.Open) text += "Жилой отсек: открыт\n"; 
-    else text += "Жилой отсек: закрыт\n"; 
+    if(MainDoor.Open) {
+        text += "Жилой отсек: открыт\n"; 
+    } else {
+        text += "Жилой отсек: закрыт\n"; 
+    }
     
-    if(FirstDoor.Open) text += "Служебный отсек: открыт\n\n";
-    else text += "Служебный отсек: закрыт\n\n";
+    if(FirstDoor.Open) {
+        text += "Служебный отсек: открыт\n\n";
+    } else {
+        text += "Служебный отсек: закрыт\n\n";
+    }
 
     // Кислород:
     text += "Кислород в отсеках:\n";
@@ -40,9 +46,11 @@ void Main()
     text += "Энергия: " + GetBattariesStatus(GetBatteries("Батарея (БК)")) + "\n\n";
 
     // HangarDoor.Open &&  MainDoor.Open
-    if(MainDoor.Open) VSpace.Depressurize = false;
-
-    else VSpace.Depressurize = true;
+    if(MainDoor.Open) {
+        VSpace.Depressurize = false;
+    } else {
+        VSpace.Depressurize = true;
+    }
     
     panel.WritePublicText(text, false);
     panel.ShowPublicTextOnScreen();
@@ -50,92 +58,86 @@ void Main()
 }
 
 // Получаем процент от числа
-float GetPercent(float min, float max) {
-
+float GetPercent(float min, float max)
+{
     return min / (max / 100f);
 }
 
 // Получаем общий статус о резервуарах
-String GetOxygenStatus(List<IMyGasTank> array) {
-
+String GetOxygenStatus(List<IMyGasTank> array)
+{
     String result = "";
-
     float middleSum = 0f;
 
     if(array.Count > 0) {
-
+        // Получаем среднее значение суммы всех резервуаров
         for (int i = 0; i < array.Count; i++) {
-
-            // Получаем среднее значение суммы всех резервуаров
             middleSum += (GetPercent(Convert.ToSingle(array[i].FilledRatio), 1f) / Convert.ToSingle(array.Count));
         }
-
         result += middleSum.ToString() + "%";
+    } else {
+        result = "Нет информации";
     }
-    
-    else result = "Нет информации";
 
     return result;
 }
 
 // Получить общий статус батарей
-String GetBattariesStatus(List<IMyBatteryBlock> array) {
-
+String GetBattariesStatus(List<IMyBatteryBlock> array)
+{
     String result = "";
-
     float middleSum = 0f;
 
     if(array.Count > 0) {
-
+        // Получаем среднее значение суммы всех резервуаров
         for (int i = 0; i < array.Count; i++) {
-
-            // middleSum += array[i].CurrentStoredPower;
             middleSum += (GetPercent(Convert.ToSingle(array[i].CurrentStoredPower), 3f) / Convert.ToSingle(array.Count));
         }
-
         result += middleSum.ToString() + "%";
+    } else {
+        result = "Нет информации";
     }
-    
-    else result = "Нет информации";
 
     return result;
 }
 
 // Получить любые резервуары по имени
-List<IMyGasTank> GetOxygenReservoirs(String name) {
-
+List<IMyGasTank> GetOxygenReservoirs(String name)
+{
     List<IMyGasTank> reservoirs = new List<IMyGasTank>();
     List<IMyGasTank> result = new List<IMyGasTank>();
 
     GridTerminalSystem.GetBlocksOfType(reservoirs);
 
     for(int i = 0; i < reservoirs.Count; i++) {
-
-        if(reservoirs[i].DisplayNameText.Equals(name)) result.Add(reservoirs[i]);
+        if(reservoirs[i].DisplayNameText.Equals(name)) {
+            result.Add(reservoirs[i]);
+        }
     }
 
     return result;
 }
 
 // Получить батареии
-List<IMyBatteryBlock> GetBatteries(String name) {
-
+List<IMyBatteryBlock> GetBatteries(String name)
+{
     List<IMyBatteryBlock> batteries = new List<IMyBatteryBlock>();
     List<IMyBatteryBlock> result = new List<IMyBatteryBlock>();
 
     GridTerminalSystem.GetBlocksOfType(batteries);
 
     for(int i = 0; i < batteries.Count; i++) {
-
-        if(batteries[i].DisplayNameText.Equals(name)) result.Add(batteries[i]);
+        if(batteries[i].DisplayNameText.Equals(name)) {
+            result.Add(batteries[i]);
+        }
     }
 
     return result;
 }
 
 // Получить ангарные двери
-List<IMyDoor> GetHangarDoor(String name) {
-
+List<IMyDoor> GetHangarDoor(String name)
+{
     // получаем все гурппы
     List<IMyBlockGroup> groups = new List<IMyBlockGroup>();
     List<IMyDoor> result = new List<IMyDoor>();
@@ -143,8 +145,9 @@ List<IMyDoor> GetHangarDoor(String name) {
     GridTerminalSystem.GetBlockGroups(groups);
 
     for(int i = 0; i < groups.Count; i++) {
-
-        if(groups[i].Name.Equals(name)) result.Add((IMyDoor)result[i]);
+        if(groups[i].Name.Equals(name)) {
+            result.Add((IMyDoor)result[i]);
+        }
     }
 
     return result;
